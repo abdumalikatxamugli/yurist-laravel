5288fa9fd2968d786d67956722eec4414e2d108a
Abdumalik <axtrem13@gmail.com>
2021-07-12 23:08:21 +0500

    init

---
+++ app/Http/Controllers/ImporterController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ImporterController extends Controller
+{
+    public function upload(Request $request){
+        print "hey";
+    }
+}
